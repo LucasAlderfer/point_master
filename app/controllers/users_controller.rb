5fb2ee2bd2cfcfa2d3ac7d18a9cdcc8c815f889b
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-07-18 17:52:58 -0600

    Adds styling to add pages, fixes user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :current_user
 
   def index
+    @users = User.all.includes(:points, :badges)
   end
 
   def show
--- spec/features/visitor_can_register_from_root_spec.rb
+++ spec/features/visitor_can_register_from_root_spec.rb
@@ -35,4 +35,30 @@ describe "visiting /" do
       expect(page).to have_content("All fields are required! No duplicate accounts allowed!")
     end
   end
+  context "as a user" do
+    it "can see all users, their current points and badges" do
+      user_1 = User.create(name:'bill', email:'anemail', password:'password')
+      user_2 = User.create(name:'jane', email:'emalia', password:'password')
+      point_1 = user_1.points.create
+      point_2 = user_2.points.create
+      point_3 = user_2.points.create
+      badge_1 = Badge.create(title:'tester')
+      user_1.user_badges.create(badge: badge_1)
+      badge_2 = Badge.create(title:'veteran')
+      user_2.user_badges.create(badge: badge_1)
+      user_2.user_badges.create(badge: badge_2)
+
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      visit users_path
+
+      expect(page).to have_content "Name: #{user_1.name}"
+      expect(page).to have_content "Name: #{user_2.name}"
+      expect(page).to have_content "Badges: #{user_1.badge_display}"
+      expect(page).to have_content "Badges: #{user_2.badge_display}"
+      expect(page).to have_content "Point Count: #{user_1.point_count}"
+      expect(page).to have_content "Point Count: #{user_2.point_count}"
+    end
+  end
 end
